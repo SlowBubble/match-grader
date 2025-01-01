bc92d69e4a86d436727647c23cc53e3b184dbe67
SlowBubble <chorhanglam@gmail.com>
2024-12-31 22:53:39 -0500

    Generate compressed chat

--- compress_chat.py
+++ compress_chat.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def extract_messages(chat_file):
     with open(chat_file, 'r') as f:
@@ -12,8 +13,11 @@ def extract_messages(chat_file):
     
     # Write one message per line
     with open('chat_compressed.json', 'w') as f:
-        json.dump(messages, f, indent=2)
-        f.write('\n')
+        for msg in messages:
+            f.write(json.dumps(msg) + '\n')
+    
+    # Delete the input file after processing
+    os.remove(chat_file)
 
 if __name__ == '__main__':
     extract_messages('chat.json')
